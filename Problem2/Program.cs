032b9bb5863eedf277128b6593be2f7b06878f15
ThomasBuck10765 <thomasbuck10765@gmail.com>
2023-07-28 19:02:33 +0100

    Problem2

---
+++ Problem2/Program.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+
+namespace Leetcode.Problem2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode returnedList = new ListNode();
+
+            returnedList.val = l1.val + l2.val;
+
+            if (l1.next == null && l2.next == null)
+            {
+                return ValueCheck(returnedList);
+            }
+
+            l1.next ??= new ListNode();
+            l2.next ??= new ListNode();
+
+            returnedList.next = AddTwoNumbers(l1.next, l2.next);
+
+            return ValueCheck(returnedList);
+        }
+
+        private static ListNode ValueCheck(ListNode node)
+        {
+            if (node.val >= 10)
+            {
+                node.next ??= new ListNode(0);
+
+                node.next.val += 1;
+                node.val -= 10;
+
+                ValueCheck(node.next);
+            }
+
+            return node;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Tests/Problem2Tests.cs
@@ -0,0 +1,80 @@
+﻿using Leetcode.Problem2;
+using System.Linq;
+using System;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace Tests
+{
+    public class Problem2Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var expected = GenerateListNode(new int[] { 7, 0, 8 });
+
+            var l1 = GenerateListNode(new int[] { 2, 4, 3 });
+            var l2 = GenerateListNode(new int[] { 5, 6, 4 });
+
+            var actual = Program.AddTwoNumbers(l1, l2);
+
+            CompareListNodes(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var expected = GenerateListNode(new int[] { 0 });
+
+            var l1 = GenerateListNode(new int[] { 0 });
+            var l2 = GenerateListNode(new int[] { 0 });
+
+            var actual = Program.AddTwoNumbers(l1, l2);
+
+            CompareListNodes(expected, actual);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var expected = GenerateListNode(new int[] { 8, 9, 9, 9, 0, 0, 0, 1 });
+
+            var l1 = GenerateListNode(new int[] { 9, 9, 9, 9, 9, 9, 9 });
+            var l2 = GenerateListNode(new int[] { 9, 9, 9, 9 });
+
+            var actual = Program.AddTwoNumbers(l1, l2);
+
+            CompareListNodes(expected, actual);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var expected = GenerateListNode(new int[] { 0, 1 });
+
+            var l1 = GenerateListNode(new int[] { 5 });
+            var l2 = GenerateListNode(new int[] { 5 });
+
+            var actual = Program.AddTwoNumbers(l1, l2);
+
+            CompareListNodes(expected, actual);
+        }
+
+        private void CompareListNodes(ListNode expected, ListNode actual)
+        {
+            Assert.AreEqual(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual));
+        }
+
+        private ListNode GenerateListNode(int[] values)
+        {
+            if (values.Length > 1)
+            {
+                return new ListNode(values[0], GenerateListNode(values.Skip(1).ToArray()));
+            }
+            else
+            {
+                return new ListNode(values[0]);
+            }
+        }
+    }
+}
