463dccbeed5595aeded81f8c24492eb00b3129e6
ThomasBuck10765 <thomasbuck10765@gmail.com>
2023-07-31 18:46:24 +0100

    Problem 5

---
+++ Problem5/Program.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using System.Text;
+
+namespace Leetcode.Problem5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static string LongestPalindrome(string s)
+        {
+            int length = s.Length;
+            string answer = s[0] + "";
+            // 1-D array to check whether substrings are palindromes or not using dynamic programming
+            bool[] paths = new bool[length];
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                string substring = s[i].ToString();
+                for (int j = length - 1; j >= i; j--)
+                {
+                    if (i == j)
+                    {
+                        paths[i] = true;
+                    }
+                    else if (j == i + 1)
+                    {
+                        paths[j] = s[i] == s[j];
+                    }
+                    else
+                    {
+                        paths[j] = paths[j - 1] && s[i] == s[j];
+                    }
+
+                    if (paths[j] && j - i + 1 > answer.Length)
+                    {
+                        answer = s.Substring(i, j - i + 1);
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Tests/Problem5Tests.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.Problem5;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Problem5Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            string expected = "aba";
+
+            string s = "babad";
+
+            var actual = Program.LongestPalindrome(s);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string expected = "bb";
+
+            string s = "cbbd";
+
+            var actual = Program.LongestPalindrome(s);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
